960e948f0ea9b30ecc4187ada04975626a478e65
kasia <kgbsz@wp.pl>
2019-03-13 19:28:31 +0100

    komentarze

--- ProgramDownload/src/main/java/DownoloadManager/DownloadManagerFactory.java
+++ ProgramDownload/src/main/java/DownoloadManager/DownloadManagerFactory.java
@@ -1,4 +1,19 @@
 package DownoloadManager;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.file.Paths;
+import java.util.Properties;
+
 public class DownloadManagerFactory {
+    public static DownloadProgram createDownloadManager(){
+        Properties properties = new Properties();
+
+        try (InputStream configInputStream = new FileInputStream(configPropertis)) {
+        }
+        return new DownloadProgram(Paths.get("C:","Users","RENT","IdeaProjects","ProgramDownload"));
+    }
 }
+// socket - sczytuje streamy, wspiera to system operacyjny
+//
+// przekierowania mozna robic z http client apache
