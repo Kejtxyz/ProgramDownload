c20f4e2c4feffb2a231ebac60550f4ed4530c374
kasia <kgbsz@wp.pl>
2019-03-13 18:54:19 +0100

    Program do sciagania stron web i zapisuje do pliku

---
+++ ProgramDownload/src/main/java/DownoloadManager/DownloadManagerFactory.java
@@ -0,0 +1,4 @@
+package DownoloadManager;
+
+public class DownloadManagerFactory {
+}
--- ProgramDownload/src/main/java/DownoloadManager/Main.java
+++ ProgramDownload/src/main/java/DownoloadManager/Main.java
@@ -1,10 +1,18 @@
 package DownoloadManager;
 
 import java.lang.reflect.MalformedParameterizedTypeException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
-    public static void main(String[] args) throws MalformedParameterizedTypeException {
-        DownloadProgram
+    public static void main(String[] args) throws MalformedURLException {
+        DownloadProgram downloadProgram = new DownloadProgram(Paths.get( "C:","Users"," RENT","IdeaProjects","ProgramDownload"));
+        URL url = new URL(args[0]);
+        downloadProgram.download(url);
+
 
     }
 }
